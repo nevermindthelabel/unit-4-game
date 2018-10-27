78a9945c91fbc607dae79572f2fcc69344a38d0d
nevermindthelabel <nevermindthelabel@gmail.com>
2018-10-27 15:21:41 -0700

    javascript updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,51 +1,67 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-function genNumber(min, max) {
-	return Math.floor((Math.random() * max) + min);
-}
+	function genNumber(min, max) {
+		return Math.floor((Math.random() * max) + min);
+	}
 	
-
-	var	firstGemNumber = genNumber(1, 12);	
-	var	secondGemNumber =  genNumber(1, 12);
-	var	thirdGemNumber =  genNumber(1, 12);
-	var	fourthGemNumber = genNumber(1, 12);;
+	var firstGemNumber = genNumber(1, 12);
+	var secondGemNumber = genNumber(1, 12);
+	var thirdGemNumber = genNumber(1, 12);
+	var fourthGemNumber = genNumber(1, 12);;
 	var randomNumber = genNumber(18, 120);
 	var totalScore = 0;
 	var wins = 0;
 	var losses = 0;
 
+	function startGame() {
 
+		randomNumber = genNumber(18, 120 - 18);
+		firstGemNumber = genNumber(1, 12);
+		secondGemNumber = genNumber(1, 12);
+		thirdGemNumber = genNumber(1, 12);
+		fourthGemNumber = genNumber(1, 12);
+		$(".randomNumber").html(randomNumber);
+		totalScore = 0;
+		$(".total").html(totalScore);
 
-	$(".gem1").on("click", function(){
+	}
+	startGame()
+
+	function checkNumber() {
+
+		if (totalScore === randomNumber) {
+			wins++;
+			$("#wins").html(wins);
+			startGame();
+		} else if (totalScore > randomNumber) {
+			losses++;
+			$("#losses").html(losses);
+			startGame();
+		} //console.log(totalScore  ,  randomNumber)
+	} 
+
+	$(".gem1").on("click", function () {
 		totalScore = totalScore + firstGemNumber;
 		$(".total").html(totalScore);
+		checkNumber();
 	})
-	$(".gem2").on("click", function(){
+	$(".gem2").on("click", function () {
 		totalScore = totalScore + secondGemNumber;
 		$(".total").html(totalScore);
+		checkNumber();
 	})
-	$(".gem3").on("click", function(){
+	$(".gem3").on("click", function () {
 		totalScore = totalScore + thirdGemNumber;
 		$(".total").html(totalScore);
+		checkNumber();
 	})
-	$(".gem4").on("click", function(){
+	$(".gem4").on("click", function () {
 		totalScore = totalScore + fourthGemNumber;
 		$(".total").html(totalScore);
+		checkNumber();
 	})
-	// create a random number between 19 and 120
-	$(".randomNumber").ready(function(){
-		randomNumber = Math.floor((Math.random() * 120) + 19);
+	$(".randomNumber").ready(function () {
 		$(".randomNumber").html(randomNumber);
 	})
 
-	$(".total").ready(function(){
-	
-	})
-	if (totalScore === randomNumber) {
-		wins++;
-		$(".wins").html(wins++)
-	} else {
-		losses++;
-		$(".losses").html(losses++);
-	}
 })
